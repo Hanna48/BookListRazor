b09427917f57dd9a98d75e910f64eea52c44399d
hanna.chung35 <hanna.chung35@TI03NB190>
2019-08-04 11:46:16 +0800

    Get all books data from DB

--- BookListRazor/BookListRazor/Pages/BookList/index.cshtml.cs
+++ BookListRazor/BookListRazor/Pages/BookList/index.cshtml.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
     public class indexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public indexModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
 
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
         }
     }
 }
